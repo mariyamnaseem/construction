cb760e1bec08c04edac7a98c4d1ca8ac09001fad
mariyamnaseem <168009857+mariyamnaseem@users.noreply.github.com>
2025-02-15 20:48:02 +0530

    Initial commit

---
+++ construction 2 - Copy/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let searchIcon = document.getElementById('search');
+    let searchForm = document.getElementById('searchform');
+    
+
+    searchIcon.addEventListener('click', function () {
+        searchForm.classList.toggle('active');
+    });
+
+    
+
+    
+
+
+});
